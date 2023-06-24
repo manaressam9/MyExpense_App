e1e3f62e1f15d0d6fbbfeda9cf194ccec8184155
manaressam9 <manareldredy@gmail.com>
2023-06-24 15:54:07 +0300

    adding GlobalStyles using styled-components lib

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,10 +1,15 @@
 import React, { StrictMode } from 'react'
-import  { createRoot }  from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './src/App.js'
+import { GlobalStyle } from './src/styles/GlobalStyle.js';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<StrictMode><App/></StrictMode>);
+root.render(
+<StrictMode>
+    <GlobalStyle />
+    <App />
+    </StrictMode>);
