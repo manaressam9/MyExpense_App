1d4c5f69d638394ae15d12188aa043a526a94705
manaressam9 <manareldredy@gmail.com>
2023-08-04 16:22:01 +0300

    set up navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React,{useMemo, useState} from "react";
 import styled from "styled-components";
 import bg from "./img/bg.jpg"
 import { MainLayout } from "./styles/Layout";
 import Orb from "./styles/Orb";
 import Navbar from "./components/Navbar";
-
+import Dashboard from "./components/Dashboard";
+import Transactions from "./components/Transactions";
+import Expenses from "./components/Expenses";
+import Income from"./components/Income"
 const App = () =>{
+    const[active, setActive] = useState(1);
+    //to avoid restarting the animation when rerendering as a result of changing active state
+    // note that if we moved ative state in navbar comp we won't need to use useMemo  
+    const orbMemo = useMemo(() => {
+       return<Orb />
+     },[])
+    //display page content  dynamicaly
+    const displayData = ()=>{
+     switch(active){
+        case 1:
+            return <Dashboard />
+        case 2:
+            return <Transactions />
+        case 3:
+            return <Income />
+        case 4:
+            return <Expenses />
+         default: 
+         return<Dashboard />
+     }
+    }
+
     return (
         <AppStyled className="App" bg={bg}>
-            <Orb />
+           
+         {orbMemo}
             <MainLayout>
-           <Navbar />
+           <Navbar active={active} setActive={setActive} />
+            <main>{displayData()}</main>
             </MainLayout>
         </AppStyled>
     )
@@ -19,6 +46,19 @@ const App = () =>{
 const AppStyled = styled.div`
 height: 100vh;
 background-image: url(${props => props.bg});
-position: relative
+position: relative;
+main{
+    flex:1;
+    background: var(--app-bg-color);
+    backdrop-filter: blur(4.5px);
+border-radius:32px ;
+border: 3px solid #fff;
+padding: 2rem 1.5rem;
+overflow: auto;
+overflow-x: hidden;
+&::-webkit-scrollbar{
+    width: 0;
+}
+}
 `;
 export default App
